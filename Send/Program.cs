b608dcf4f767ea76413a61085d7fabe94e1cd2d4
Minke <111324980+MinkeCrafford@users.noreply.github.com>
2024-02-21 20:38:36 +0000

    Add/Edit comments

--- Send/Program.cs
+++ Send/Program.cs
@@ -1,9 +1,14 @@
-﻿using System.Text;
-using RabbitMQ.Client;
+﻿// Added library
+using System.Text; // Library for encoding and decoding text
+using RabbitMQ.Client; // Library for interacting with RabbitMQ
 
-// Create Connection to RabbitMQ service
+// Create connection factory to connect to RabbitMQ service
 var factory = new ConnectionFactory { HostName = "localhost"};
+
+// Establish a connection to the RabbitMQ service
 using var connection = factory.CreateConnection();
+
+// Create a channel, which is used for communication with RabbitMQ
 using var channel = connection.CreateModel();
 
 // Declare a message queue
@@ -13,22 +18,23 @@ channel.QueueDeclare(queue: "nameQueue",
                     autoDelete: false,
                     arguments: null);
 
-// Print to console
+// Print to console (promt user to enter name)
 Console.WriteLine("Enter your name:");
 
-// Store input from user 
+// Read and store input from user 
 var name = Console.ReadLine();
 
-// Create message string with input from name string 
+// Create message string with input from name
 string message = $"Hello my name is, {name}";
 
-//Print string
+//Print message to console 
 Console.WriteLine(message);
 
-//Encode message 
+//Encode message from sting to bytes using UTF-8
 var encodedMessage = Encoding.UTF8.GetBytes(name!); // The '!' is a null forgiving operator 
 
 //Send message to receiver 
+// Publish the encoded message to the "nameQueue" queue
 channel.BasicPublish("",
                     "nameQueue", 
                     null, 
